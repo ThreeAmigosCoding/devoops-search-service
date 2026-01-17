4fa7de5dfc78bf27fef23e499fd180e59501ee11
Dusan <dusan_rozic@hotmail.co.uk>
2026-01-17 22:01:49 +0100

    feat: Add zipkin tracing

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 	implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    //zipkin(tracing)
+    implementation("org.springframework.boot:spring-boot-micrometer-tracing-brave")
+    implementation("org.springframework.boot:spring-boot-starter-zipkin")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
 //	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
